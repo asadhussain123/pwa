66e05fef622188d0eed9cf947e3cebfaf6575ca5
AN <asadh113@live.com>
2019-12-01 23:37:44 +0500

    updated header

--- src/app/common/translation.ts
+++ src/app/common/translation.ts
@@ -8,9 +8,10 @@ export const Translation = {
             email: "Enter Email",
             address: "Enter Address",
             name: "Enter Name",
-            submitBtn: "Signup",
+            submitBtn: "Submit",
             numbersOnlyMsg: "Please enter valid number",
-            validEmail: "Please Enter valid email Address"
+            validEmail: "Please Enter valid email Address",
+            register: "Register"
         },
         appLayout: {
             language: "Language"
@@ -27,7 +28,8 @@ export const Translation = {
             name: "نام درج کریں",
             submitBtn: "جمع",
             numbersOnlyMsg: "براہ کرم درست نمبر درج کریں",
-            validEmail: "براہ کرم درست ای میل ایڈریس درج کریں"
+            validEmail: "براہ کرم درست ای میل ایڈریس درج کریں",
+            register: "رجسٹر کریں"
         },
         appLayout: {
             language: "زبان"
